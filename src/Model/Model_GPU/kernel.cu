9f15af32ceed1ba0cfd32c3d4c91f520c585769c
Yannig <yannig.charonnat2@imt-atlantique.net>
2026-03-11 15:01:08 +0100

    avant la suppression de la masse

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -3,11 +3,11 @@
 #include "cuda.h"
 #include "kernel.cuh"
 #include <iostream>
+#include <cmath>
 #define DIFF_T (0.1f)
 #define EPS (1.0f)
 
 __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float* massesGPU, int n_particles)
-//__global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
 {
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -17,38 +17,27 @@ __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float
 
 	float3 acc = {0.0f, 0.0f, 0.0f};
 
-	// accelerationsGPU[i].x = 0;
-	// accelerationsGPU[i].y = 0;
-	// accelerationsGPU[i].z = 0;
-
 	for (int j = 0; j < n_particles; j++)
 	{
-		if(i != j)
+		const float diffx = positionsGPU[j].x - positionsGPU[i].x;
+		const float diffy = positionsGPU[j].y - positionsGPU[i].y;
+		const float diffz = positionsGPU[j].z - positionsGPU[i].z;
+
+		float dij = fmaf(diffx, diffx, fmaf(diffy, diffy, fmaf(diffz, diffz, 0.0f)));
+
+		if (dij < 1.0)
 		{
-			const float diffx = positionsGPU[j].x - positionsGPU[i].x;
-			const float diffy = positionsGPU[j].y - positionsGPU[i].y;
-			const float diffz = positionsGPU[j].z - positionsGPU[i].z;
-
-			float dij = diffx * diffx + diffy * diffy + diffz * diffz;
-
-			if (dij < 1.0)
-			{
-				dij = 10.0;
-			}
-			else
-			{
-				dij = std::sqrt(dij);
-				dij = 10.0 / (dij * dij * dij);
-			}
-
-			// accelerationsGPU[i].x += diffx * dij * massesGPU[j];
-			// accelerationsGPU[i].y += diffy * dij * massesGPU[j];
-			// accelerationsGPU[i].z += diffz * dij * massesGPU[j];
-
-			acc.x += diffx * dij * massesGPU[j];
-			acc.y += diffy * dij * massesGPU[j];
-			acc.z += diffz * dij * massesGPU[j];
+			dij = 10.0;
 		}
+		else
+		{
+			dij = rsqrtf(dij);
+			dij = 10.0 * (dij * dij * dij);
+		}
+
+		acc.x += diffx * dij * massesGPU[j];
+		acc.y += diffy * dij * massesGPU[j];
+		acc.z += diffz * dij * massesGPU[j];
 	}
 
 	velocitiesGPU[i].x += acc.x * 2.0f;
@@ -58,7 +47,6 @@ __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float
 }
 
 __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, int n_particles)
-//__global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, int n_particles)
 {
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 	
@@ -66,24 +54,22 @@ __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, int n_par
 		return;
 	}
 
-	// velocitiesGPU[i].x += accelerationsGPU[i].x * 2.0f;
-	// velocitiesGPU[i].y += accelerationsGPU[i].y * 2.0f;
-	// velocitiesGPU[i].z += accelerationsGPU[i].z * 2.0f;
-	positionsGPU[i].x += velocitiesGPU[i].x * 0.1f;
-	positionsGPU[i].y += velocitiesGPU[i].y * 0.1f;
-	positionsGPU[i].z += velocitiesGPU[i].z * 0.1f;
+	// positionsGPU[i].x += velocitiesGPU[i].x * 0.1f;
+	// positionsGPU[i].y += velocitiesGPU[i].y * 0.1f;
+	// positionsGPU[i].z += velocitiesGPU[i].z * 0.1f;
+
+	positionsGPU[i].x = std::fmaf(velocitiesGPU[i].x, 0.1f, positionsGPU[i].x);
+	positionsGPU[i].y = std::fmaf(velocitiesGPU[i].y, 0.1f, positionsGPU[i].y);
+	positionsGPU[i].z = std::fmaf(velocitiesGPU[i].z, 0.1f, positionsGPU[i].z);
 
 }
 
-//void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* accelerationsGPU, float* massesGPU, int n_particles)
 void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float* massesGPU, int n_particles)
 {
 	int nthreads = 32;
 	int nblocks =  (n_particles + (nthreads -1)) / nthreads;
 
 	compute_acc<<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, massesGPU, n_particles);
-	//compute_acc<<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, massesGPU, n_particles);
-	//maj_pos    <<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, accelerationsGPU, n_particles);
 	maj_pos    <<<nblocks, nthreads>>>(positionsGPU, velocitiesGPU, n_particles);
 }
 
